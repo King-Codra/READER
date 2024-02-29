4607f39f5c25adf6aa85a1649cd595798c43d419
King-Codra <133504098+King-Codra@users.noreply.github.com>
2024-02-29 20:10:04 +0100

    Refined fetchBookInfo function (#1)
    Added functionality to change HTML accordingly & find Coverphoto
    Added functionality to change Language CODE to word

--- script.js
+++ script.js
@@ -3,35 +3,87 @@ const apiKey = process.env.API_KEY;
 const submitBtn = document.getElementById('submitBtn');
 const ISBNInputField = document.getElementById('isbnInput');
 
+function getFullLanguageName(languageCode) {
+  const languageDisplay = new Intl.DisplayNames(['en'], { type: 'language' });
+  return languageDisplay.of(languageCode);
+}
+
 // Function to fetch book information using ISBN
 async function fetchBookInfo(isbn) {
   try {
     const response = await axios.get(
       `https://www.googleapis.com/books/v1/volumes?q=isbn:${isbn}&key=${apiKey}`
     );
-    const bookData = response.data;
-    console.log(`bookData:`, bookData);
+    const data = response.data;
+    // Check if items array exists and has at least one item
+    if (!data.items || data.items.length === 0) {
+      console.log('No books found for this ISBN.');
+      return; // Exit the function if no books are found
+    }
+
+    console.log(`data:`, data);
+
+    const volumeInfo = data.items[0].volumeInfo;
 
     // Return object with above info
-    return {
-      authors: bookData.items[0].volumeInfo.authors.join(', '),
-      pages: bookData.items[0].volumeInfo.pageCount,
-      releaseDate: bookData.items[0].volumeInfo.publishedDate,
-      language: bookData.items[0].volumeInfo.language,
-      title: bookData.items[0].volumeInfo.title,
+    const book = {
+      authors: volumeInfo.authors ? volumeInfo.authors.join(', ') : 'Unknown',
+      pages: volumeInfo.pageCount > 0 ? volumeInfo.pageCount : 'Unkown',
+      releaseDate: volumeInfo.publishedDate
+        ? volumeInfo.publishedDate
+        : 'Unknown',
+      language: volumeInfo.language ? volumeInfo.language : 'Unknown',
+      title: volumeInfo.title ? volumeInfo.title : 'Unknown',
+      cover:
+        volumeInfo.imageLinks?.thumbnail ||
+        new URL('no-cover.png', import.meta.url).toString(),
     };
+    return book;
   } catch (error) {
     console.error('Error fetching book information:', error);
     throw error;
   }
 }
 
+class BookView {
+  #parentElement = document.querySelector('.book-details');
+  #data;
+
+  render(data) {
+    this.#data = data;
+    const markup = this.#generateMarkup();
+    this.#clear();
+    this.#parentElement.insertAdjacentHTML('afterbegin', markup);
+  }
+
+  #clear() {
+    this.#parentElement.innerHTML = '';
+  }
+
+  #generateMarkup() {
+    return `
+  
+    <img src="${this.#data.cover}" alt="Book Cover" class="book-cover" />
+    <div class="book-details">
+      <h2 class="book-title">${this.#data.title}</h2>
+      <p class="book-author">${this.#data.authors}</p>
+      <p class="book-release-date">Year: ${this.#data.releaseDate}</p>
+      <p class="book-pages">Pages: ${this.#data.pages}</p>
+      <p class="book-language">${getFullLanguageName(
+        `${this.#data.language}`
+      )}</p>
+`;
+  }
+}
+
+const bookView = new BookView();
+
 // Button click event handler that "searches" and updates according to passed in ISBN
 submitBtn.addEventListener('click', () => {
   const isbn = ISBNInputField.value;
   fetchBookInfo(isbn)
     .then((bookInfo) => {
-      console.log(bookInfo);
+      bookView.render(bookInfo);
     })
     .catch((error) => {
       console.error('Error fetching book information:', error);
